6b29a059ff9f062c6b7a639aa74365b430244d93
JC Silverio <silverio@impulsevfx.com>
2017-11-03 23:40:16 -0400

    players can be removed

--- app.jsx
+++ app.jsx
@@ -118,6 +118,7 @@ function Player(props) {
   return (
       <div className="player">
               <div className="player-name">
+              <a className="remove-player" onClick={props.onRemove}>✕</a>
               {props.name}
               </div>
               <div className="player-score">
@@ -133,6 +134,7 @@ Player.propTypes = {
   name: React.PropTypes.string.isRequired,
   score: React.PropTypes.number.isRequired,
   onScoreChange: React.PropTypes.func.isRequired,
+  onRemove: React.PropTypes.func.isRequired,
 }
 
 var Application = React.createClass({
@@ -159,13 +161,11 @@ var Application = React.createClass({
   },
 
   onScoreChange: function (index, delta) {
-    console.log('onScoreChange: ', index, delta);
     this.state.players[index].score += delta;
     this.setState(this.state);
   },
 
   onPlayerAdd: function (name) {
-    console.log('Player Added ', name);
     this.state.players.push({
       name: name,
       score: 0,
@@ -173,6 +173,12 @@ var Application = React.createClass({
     });
     this.setState(this.state);
     nextId++;
+  },
+
+  onRemovePlayer: function (index) {
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+
   },
 
   render: function () {
@@ -185,6 +191,7 @@ var Application = React.createClass({
               return (
                 <Player
                     onScoreChange={function(delta) {this.onScoreChange(index,delta)}.bind(this)}
+                    onRemove={function() {this.onRemovePlayer(index)}.bind(this)}
                     name={player.name}
                     score={player.score}
                     key={player.id} />
